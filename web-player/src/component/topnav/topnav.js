8f6237c7e22c2eddbff5c3f425dddeba4faa0f09
Aryan Chavan <aryan@aryansdomain.com>
2022-03-27 01:33:20 +0530

    make homepage login specific

--- web-player/src/component/topnav/topnav.js
+++ web-player/src/component/topnav/topnav.js
@@ -1,28 +1,61 @@
-import PrevPageBtn from '../buttons/prev-page-button';
-import NextPageBtn from '../buttons/next-page-button';
-import SearchBox from './search-box';
-import LibraryTabBtn from './library-tab-btn';
+import PrevPageBtn from "../buttons/prev-page-button";
+import NextPageBtn from "../buttons/next-page-button";
+import SearchBox from "./search-box";
+import LibraryTabBtn from "./library-tab-btn";
 
-import styles from './topnav.module.css';
+import styles from "./topnav.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import config from "../../config";
 
-function Topnav({search = false, tabButtons = false}) {
-    return (
-      <nav className={styles.Topnav}>
-          <div>
-                <span>
-                    <PrevPageBtn />
-                    <NextPageBtn />
-                    { search ? <SearchBox /> : '' }
-                    { tabButtons ? <LibraryTabBtn /> : '' }
-                </span>
-                <span>
-                    <button className={styles.ProfileBtn}>
-                        Sign In
-                    </button>
-                </span>
-          </div>
-      </nav>
-    );
+function Topnav({ search = false, tabButtons = false }) {
+	const [user, setUser] = useState(false);
+
+	useEffect(() => {
+		if (localStorage.token) {
+			axios
+				.get(`${config.api_location}/user/self`, {
+					headers: { token: localStorage.token },
+				})
+				.then((res) => {
+					setUser(res.data);
+				});
+		}
+	}, []);
+	return (
+		<nav className={styles.Topnav}>
+			<div>
+				<span>
+					<PrevPageBtn />
+					<NextPageBtn />
+					{search ? <SearchBox /> : ""}
+					{tabButtons ? <LibraryTabBtn /> : ""}
+				</span>
+				<span>
+					{user ? (
+						<button
+							className={styles.ProfileBtn}
+							onClick={() => {
+								localStorage.clear();
+								window.location = "/";
+							}}
+						>
+							Log Out
+						</button>
+					) : (
+						<button
+							className={styles.ProfileBtn}
+							onClick={() => {
+								window.location = "/register";
+							}}
+						>
+							Register
+						</button>
+					)}
+				</span>
+			</div>
+		</nav>
+	);
 }
-  
+
 export default Topnav;
--- web-player/src/pages/home.js
+++ web-player/src/pages/home.js
@@ -1,57 +1,70 @@
-import Topnav from '../component/topnav/topnav';
-import TitleL from '../component/text/title-l';
-import TitleM from '../component/text/title-m'
-import PlaylistCardS from '../component/cards/playlist-card-s';
-import PlaylistCardM from '../component/cards/playlist-card-m';
+import Topnav from "../component/topnav/topnav";
+import TitleL from "../component/text/title-l";
+import TitleM from "../component/text/title-m";
+import PlaylistCardS from "../component/cards/playlist-card-s";
+import PlaylistCardM from "../component/cards/playlist-card-m";
 
 import styles from "./home.module.css";
 
-import { PLAYLIST } from '../data/index'
+import { PLAYLIST } from "../data/index";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import config from "../config";
 
-function Home(){
-    return (
-        <div className={styles.Home}>
-            <div className={styles.HoverBg}></div>
-            <div className={styles.Bg}></div>
+function Home() {
+	const [user, setUser] = useState(false);
 
-            <Topnav />
-            <div className={styles.Content}>
-                <section>
-                    <div className={styles.SectionTitle}>
-                        <TitleL>Have a nice day!</TitleL>
-                    </div>
+	useEffect(() => {
+		if (localStorage.token) {
+			axios
+				.get(`${config.api_location}/user/self`, {
+					headers: { token: localStorage.token },
+				})
+				.then((res) => {
+					setUser(res.data);
+				});
+		}
+	}, []);
 
-                    <div className={styles.SectionCards}>
-                        {PLAYLIST.map((item) => {
-                            return (
-                                <PlaylistCardS 
-                                    key={item.title}
-                                    data={item}
-                                />
-                            );
-                        })}
-                    </div>
-                </section>
+	return (
+		<div className={styles.Home}>
+			<div className={styles.HoverBg}></div>
+			<div className={styles.Bg}></div>
 
-                <section>
-                    <div className={styles.SectionTitle}>
-                        <TitleM>Recently Played</TitleM>
-                    </div>
-                    
-                    <div className={styles.SectionCardsMedium}>
-                        {PLAYLIST.slice(0, 6).map((item) => {
-                            return (
-                                <PlaylistCardM 
-                                    key={item.title}
-                                    data={item}
-                                />
-                            );
-                        })}
-                    </div>
-                </section>
-            </div>
-        </div>
-    );
+			<Topnav />
+			<div className={styles.Content}>
+				<section>
+					<div className={styles.SectionTitle}>
+						<TitleL>
+							{user
+								? `Hello ${user.username}`
+								: "Sign in to get personalized reccomendations!"}
+						</TitleL>
+					</div>
+
+					<div className={styles.SectionCards}>
+						{PLAYLIST.map((item) => {
+							return <PlaylistCardS key={item.title} data={item} />;
+						})}
+					</div>
+				</section>
+
+				{user ? (
+					<section>
+						<div className={styles.SectionTitle}>
+							<TitleM>Recently Played</TitleM>
+						</div>
+
+						<div className={styles.SectionCardsMedium}>
+							{PLAYLIST.slice(0, 6).map((item) => {
+								return <PlaylistCardM key={item.title} data={item} />;
+							})}
+						</div>
+					</section>
+				) : null}
+			</div>
+		</div>
+	);
 }
 
 export default Home;
