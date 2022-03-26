9c5429d8b3a98da73d276405154df88d434f7c49
Aryan Chavan <aryan@aryansdomain.com>
2022-03-27 00:57:43 +0530

    add login

--- web-player/src/pages/login.js
+++ web-player/src/pages/login.js
@@ -1,33 +1,60 @@
+import axios from "axios";
 import React, { Component } from "react";
+import config from "../config";
 
-function Login(){
-    return (
-        <form>
-            <h3>Sign In</h3>
+function Login() {
+	return (
+		<form
+			onSubmit={(e) => {
+				e.preventDefault();
+				const data = {
+					username: e.target.username.value,
+					password: e.target.password.value,
+				};
 
-            <div className="form-group">
-                <label>Email address</label>
-                <input type="email" className="form-control" placeholder="Enter email" />
-            </div>
+				console.log({ data });
 
-            <div className="form-group">
-                <label>Password</label>
-                <input type="password" className="form-control" placeholder="Enter password" />
-            </div>
+				axios
+					.post(`${config.api_location}/auth/login`, data)
+					.then((res) => {
+						console.log(res.data.token);
+						localStorage.token = res.data.token;
+						window.location = "/";
+					})
+					.catch((err) => {
+						alert(err.response.data.msg);
+					});
+			}}
+		>
+			<h3>Sign In</h3>
 
-            <div className="form-group">
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                </div>
-            </div>
+			<div className="form-group">
+				<label>Username</label>
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Enter username"
+					name="username"
+					required
+				/>
+			</div>
 
-            <button type="submit" className="btn-btn-primary-btn-block">Submit</button>
-            <p className="forgot-password text-right">
-                Forgot <a href="#">password?</a>
-            </p>
-        </form>
-    );
+			<div className="form-group">
+				<label>Password</label>
+				<input
+					type="password"
+					className="form-control"
+					placeholder="Enter password"
+					name="password"
+					required
+				/>
+			</div>
+
+			<button type="submit" className="btn-btn-primary-btn-block">
+				Submit
+			</button>
+		</form>
+	);
 }
 
 export default Login;
