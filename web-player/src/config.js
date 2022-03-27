aba233c80df1d76baae1c15d76fc0cb269563962
Aryan Chavan <aryan@aryansdomain.com>
2022-03-27 06:31:45 +0530

    fix ip

--- web-player/src/config.js
+++ web-player/src/config.js
@@ -1,6 +1,6 @@
 const config = {
 	api_location: "http://192.168.98.134:5050",
-	ipfs_location: "http://192.168.98.242.220:8080",
+	ipfs_location: "http://192.168.98.242:8080",
 };
 
 export default config;
